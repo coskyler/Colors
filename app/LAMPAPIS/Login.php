a62df229aaed580b767ccef1dc82d451a1a0665e
Skyler Quinby <skylerpquinby@gmail.com>
2025-09-01 04:59:23 -0400

    Redirect from login if already logged in

--- app/LAMPAPIS/Login.php
+++ app/LAMPAPIS/Login.php
@@ -1,4 +1,11 @@
 <?php
+require_once __DIR__ . '/../cookie.php';
+
+if(checkAuthCookie() !== null) {
+    header("Location: /MyColors.php");
+    exit;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     include $_SERVER['DOCUMENT_ROOT'] . "/html/Login.html";
 }
