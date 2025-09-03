5f1048a224fd46dea8c77676a9962deb3f9d1c81
Skyler Quinby <skylerpquinby@gmail.com>
2025-09-03 19:38:21 -0400

    Add logout

--- app/LAMPAPIS/SearchColors.php
+++ app/LAMPAPIS/SearchColors.php
@@ -1,4 +1,13 @@
 <?php
+require_once __DIR__ . '/../cookie.php';
+
+if(checkAuthCookie() === null) {
+    header("Location: /LAMPAPIS/Login.php");
+    exit;
+}
+
+header('Cache-Control: no-store');
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     include $_SERVER['DOCUMENT_ROOT'] . "/html/MyColors.html";
 }
